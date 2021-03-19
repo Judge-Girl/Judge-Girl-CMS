b90c2dc50e28b8e16c7d02675c9bbc7b82f06765
chiyuyu <42633015+chiyuyu@users.noreply.github.com>
2021-03-19 12:51:17 +0800

    delete unnecessary comments

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,3 @@
 export function isLegalText(text, textList) {
     return !text || /^\s*$/.test(text) || textList.some(e => e.text === text);
 }
-
-// const isLegalText = (text, textList) => {
-//     return !text || /^\s*$/.test(text) || textList.some(e => e.text === text);
-// }

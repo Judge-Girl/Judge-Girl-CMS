21bc2db37fae2639d69d3089535249284b6d7a11
Chaoyuuu <chaoyu2330@gmail.com>
2021-04-07 21:32:58 +0800

    Refactor to function link

--- src/app/exam/Examinees.js
+++ src/app/exam/Examinees.js
@@ -2,18 +2,19 @@ import './Examinees.css';
 import * as React from "react";
 import {FaRegEdit, FaUserFriends} from 'react-icons/fa'
 import {AiOutlineSetting} from 'react-icons/ai'
-import FakeLink from "../commons/FakeLink";
-import {ItemListPage} from "../commons/ItemListPage/ItemListPage";
 import {NavLink} from "react-router-dom";
+import {withRouter} from "react-router";
 
-function link(currentPathName, to, name, className) {
+function link(currentPathName, to, name, Icon, isActive) {
     return (
-        <NavLink to={to} activeClassName={`active-link ${className}`} className={className}
-                 isActive={() => currentPathName.startsWith(to)}>{name}</NavLink>
+        <NavLink to={to} className={isActive ? "is-active": ""}>
+            <Icon className="tab-icon"/>{name}</NavLink>
     );
 }
 
-const Examinees = function () {
+const Examinees = withRouter(({history}) => {
+    const currentPathName = history.location.pathname;
+
     return (
         <div>
             <div className="index-container">
@@ -21,24 +22,13 @@ const Examinees = function () {
                     <a> Exam </a><span> / </span><a> 2021 Sample-Exam </a>
                 </p>
                 <div className="tabs">
-                    <ul>
-                        <li className="is-active">
-                            <a><FaRegEdit className="tab-icon"/>Problems</a>
-                        </li>
-                        <li>
-                            <a><FaUserFriends className="tab-icon"/>Participants</a>
-                        </li>
-                        <li>
-                            <a><AiOutlineSetting className="tab-icon"/>Options</a>
-                        </li>
-                    </ul>
+                    {link(currentPathName, '/exams/problems', 'Problems', FaRegEdit, true)}
+                    {link(currentPathName, '/exams/participants', 'Participants', FaUserFriends, false)}
+                    {link(currentPathName, '/exams/options', 'Options', AiOutlineSetting, false)}
                 </div>
             </div>
-
         </div>
-
-    )
-
-};
+    );
+});
 
 export {Examinees};

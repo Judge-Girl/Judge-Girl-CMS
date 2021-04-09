b542560acdcae1d5aaf5683955f3a94656447982
Chaoyuuu <chaoyu2330@gmail.com>
2021-04-09 22:40:31 +0800

    Extract to AddParticipantModal

--- src/app/exam/Examinees.js
+++ src/app/exam/Examinees.js
@@ -5,64 +5,80 @@ import FakeLink from "../commons/FakeLink";
 import {studentService} from "../../services/services";
 import {ItemListPage} from "../commons/ItemListPage/ItemListPage";
 import {DropDownBtn} from "../commons/buttons/DropDownButton";
-import {AddStudentModal} from "./modals/AddStudentModal";
-import {AddGroupModal} from "./modals/AddGroupModal";
+import {AiOutlineMail, AiOutlineUsergroupAdd} from "react-icons/ai";
+import {AddParticipantModal} from "./modals/AddParticipantModal";
 
 const Examinees = withRouter(({history}) => {
-    const currentPathName = history.location.pathname;
-    const [students, setStudents] = useState(undefined);
-    const [showAddStudentModal, setShowAddStudentModal] = useState(false);
-    const [showAddGroupModal, setShowAddGroupModal] = useState(false);
+        const currentPathName = history.location.pathname;
+        const [students, setStudents] = useState(undefined);
+        const [showAddStudentModal, setShowAddStudentModal] = useState(false);
+        const [showAddGroupModal, setShowAddGroupModal] = useState(false);
 
-    useEffect(() => {
-        if (!students) {
-            studentService.getStudents({skip: 0, size: 100})
-                .then(students => setStudents(students));
-        }
-    });
+        useEffect(() => {
+            if (!students) {
+                studentService.getStudents({skip: 0, size: 100})
+                    .then(students => setStudents(students));
+            }
+        });
 
-    return (
-        <div>
-            <ExamHome currentPathName={currentPathName} examName={"2021 Sample-Exam"}/>
+        return (
+            <div>
+                <ExamHome currentPathName={currentPathName} examName={"2021 Sample-Exam"}/>
 
-            <div style={{padding: "40px 15rem 20px 15rem"}}>
-                <ItemListPage title="Participants"
-                              filterItems={["Filter", "Name", "Email"]}
-                    // TODO: connect to AddStudentModal / AddGroupModal
-                              Button={() => new DropDownBtn({
-                                  buttonName: '+ People',
-                                  dropDownItems: [
-                                      {
-                                          name: "Student",
-                                          onClick: () => setShowAddStudentModal(true)
-                                      },
-                                      {
-                                          name: "Group",
-                                          onClick: () => setShowAddGroupModal(true)
-                                      }
+                <div style={{padding: "40px 15rem 20px 15rem"}}>
+                    <ItemListPage title="Participants"
+                                  filterItems={["Filter", "Name", "Email"]}
+                                  Button={() => new DropDownBtn({
+                                      buttonName: '+ People',
+                                      dropDownItems: [
+                                          {
+                                              name: "Student",
+                                              onClick: () => setShowAddStudentModal(true)
+                                          },
+                                          {
+                                              name: "Group",
+                                              onClick: () => setShowAddGroupModal(true)
+                                          }
 
-                                  ]
-                              })}
-                              tableHeaders={["Name", "Email", " "]}
-                              tableRowGenerator={{
-                                  list: students,
-                                  key: (student) => student.id,
-                                  data: (student) => [
-                                      (<FakeLink content={student.name}/>),
-                                      student.email,
-                                      ""
-                                  ]
-                              }}
-                              tableDataStyle={{textAlign: "left"}}/>
+                                      ]
+                                  })}
+                                  tableHeaders={["Name", "Email", " "]}
+                                  tableRowGenerator={{
+                                      list: students,
+                                      key: (student) => student.id,
+                                      data: (student) => [
+                                          (<FakeLink content={student.name}/>),
+                                          student.email,
+                                          ""
+                                      ]
+                                  }}
+                                  tableDataStyle={{textAlign: "left"}}/>
+                </div>
+
+                <AddParticipantModal title={"Add Students"}
+                                     content={{
+                                         description: "Add students to the exam with the students’ email.",
+                                         Icon: AiOutlineMail,
+                                         placeholder: "studentA@example.com\nstudentB@example.com",
+                                         remark: "＊One email per line.",
+                                         buttonName: "Add"
+                                     }}
+                                     show={showAddStudentModal}
+                                     onClose={() => setShowAddStudentModal(false)}/>
+
+                <AddParticipantModal title={"Add Students By Groups"}
+                                     content={{
+                                         description: "Add groups to the exam with the groups’ name.",
+                                         Icon: AiOutlineUsergroupAdd,
+                                         placeholder: "group-name-A\ngroup-name-B",
+                                         remark: "＊One group name per line.",
+                                         buttonName: "Add"
+                                     }}
+                                     show={showAddGroupModal}
+                                     onClose={() => setShowAddGroupModal(false)}/>
             </div>
-
-            <AddStudentModal show={showAddStudentModal}
-                             onClose={() => setShowAddStudentModal(false)}/>
-
-            <AddGroupModal show={showAddGroupModal}
-                           onClose={() => setShowAddGroupModal(false)}/>
-        </div>
-    );
-});
+        );
+    }
+);
 
 export {Examinees};
--- src/app/exam/modals/AddParticipantModal.js
+++ src/app/exam/modals/AddParticipantModal.js
@@ -1,30 +1,29 @@
-import './AddStudentModal.scss'
+import './AddParticipantModal.scss'
 import React, {createRef} from "react";
 import {renderModal} from "../../commons/modals/modal";
 import {ModalHeader} from "../../commons/modals/ModalHeader";
-import {AiOutlineMail} from "react-icons/ai"
 
-const AddStudentModal = ({show, onClose}) => {
+const AddParticipantModal = ({title, content, show, onClose}) => {
     const closeIconRef = createRef(), formRef = createRef();
+    const Icon = content.Icon;
 
     const handleSubmit = e => {
         e.preventDefault();
     };
 
     return renderModal({
-        modalClassName: "add-student-modal",
+        modalClassName: "add-participant-modal",
         modalWidth: "660px",
         show, onClose, closeIconRef,
         contentRendering: () => (
             <form onSubmit={handleSubmit} ref={formRef}>
                 <div id="modal" className="font-poppins has-text-centered">
-                    <ModalHeader className="header" title="Add Students" textAlign="left"/>
-                    <p className="description"><AiOutlineMail className="modal-icon"/>Add students to the exam with the
-                        students’ email.</p>
-                    <textarea className="textarea" placeholder="studentA@example.com &#10;studentB@example.com"/>
+                    <ModalHeader className="header" title={title} textAlign="left"/>{content.icon}
+                    <p className="description"><Icon className="modal-icon"/>{content.description}</p>
+                    <textarea className="textarea" placeholder={content.placeholder}/>
                     <div className="columns">
                         <div className="column">
-                            <p className="note">＊One email per line.</p>
+                            <p className="remark">{content.remark}</p>
                         </div>
                         <div className="column">
                             <button className="button mt-5" id="add-btn">Add</button>
@@ -37,4 +36,4 @@ const AddStudentModal = ({show, onClose}) => {
 };
 
 
-export {AddStudentModal}
+export {AddParticipantModal}

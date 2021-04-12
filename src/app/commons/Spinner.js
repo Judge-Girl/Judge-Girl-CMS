efd1c2a3dc6a1b06688a0bcb2bcb0cbda036fc59
Chaoyuuu <chaoyu2330@gmail.com>
2021-04-12 20:35:10 +0800

    Improve spinner.scss

--- src/app/commons/Spinner.js
+++ src/app/commons/Spinner.js
@@ -4,10 +4,10 @@ import * as React from "react";
 const Spinner = function () {
     return (
         <div className="sk-folding-cube">
-            <div className="sk-cube1 sk-cube"></div>
-            <div className="sk-cube2 sk-cube"></div>
-            <div className="sk-cube4 sk-cube"></div>
-            <div className="sk-cube3 sk-cube"></div>
+            <div className="sk-cube1 sk-cube"/>
+            <div className="sk-cube2 sk-cube"/>
+            <div className="sk-cube4 sk-cube"/>
+            <div className="sk-cube3 sk-cube"/>
         </div>
     )
 };

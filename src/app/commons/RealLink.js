24f6ae7ea2f875080d8d711e3902926b1d65fa30
ningwang <nyngwang@gmail.com>
2021-04-27 22:17:25 +0800

    Remove redundant.

--- src/app/commons/RealLink.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export default function RealLink({content}) {
-  return (
-    <span style={{color: "#1273BA", cursor: "pointer"}}
-          onClick={e => {
-          }}> {content} </span>
-  )
-}

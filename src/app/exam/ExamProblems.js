bf800d5eb1299b1d40d617a6fe6786f887f91070
edisonhello <edisonhello@hotmail.com>
2021-04-28 14:47:00 +0800

    Frontend components

--- src/app/exam/ExamProblems.js
+++ src/app/exam/ExamProblems.js
@@ -1,13 +1,40 @@
-import {ExamInPageNavigationBar} from "./ExamInPageNavigationBar";
-import {withRouter} from "react-router";
+import { ExamInPageNavigationBar } from "./ExamInPageNavigationBar";
+import { withRouter } from "react-router";
+import { ItemListPage } from "../commons/ItemListPage/ItemListPage.js";
+import { examService } from "../../services/services.js";
+import { useState } from "react";
 
-const ExamProblems = withRouter(({history}) => {
+
+const ExamProblems = withRouter(({ history, match }) => {
     const currentPathName = history.location.pathname;
+    const [problems, setProblems] = useState([]);
+
+    const examId = match.params.examId;
+
+    examService.getExam(examId).then(exam => {
+        setProblems(exam.questions);
+    });
+
     return (
         <div>
-            <ExamInPageNavigationBar currentPathName={currentPathName} examName={"2021 Sample-Exam"}/>
+            <ExamInPageNavigationBar currentPathName={currentPathName} examName={"2021 Sample-Exam"} />
+
+            <div style={{ padding: "40px 15rem 20px 15rem" }}>
+                <ItemListPage
+                    title="Problems"
+                    tableHeaders={["#", "Problem Title", "Score Percentage", "Submission Quota", " "]}
+                    tableRowGenerator={{
+                        list: problems,
+                        key: (problem) => problem.id,
+                        data: (problem) => [
+                            1, 2, 3, 4, 5,
+                        ]
+                    }}
+                    showFilterSearchBar={false}
+                    tableDataStyle={{ textAlign: "left" }} />
+            </div>
         </div>
     )
 });
 
-export {ExamProblems};
+export { ExamProblems };
--- src/models/Exam.js
+++ src/models/Exam.js
@@ -1,9 +1,10 @@
 export default class Exam {
-    constructor({id, name, description, startTime, endTime}) {
+    constructor({ id, name, description, startTime, endTime, questions }) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.questions = questions;
     }
 }

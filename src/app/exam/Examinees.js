26f6f9ded30927cbcd5fbaeb78a6e75f518a262f
Chaoyuuu <chaoyu2330@gmail.com>
2021-04-29 16:42:24 +0800

    Fixed :(((

--- src/app/exam/Examinees.js
+++ src/app/exam/Examinees.js
@@ -67,12 +67,12 @@ const Examinees = withRouter(({history, match}) => {
         }
 
         function removeExamineeByEmail(email) {
-            const examineesArray = removeIf(examinees, examinee => examinee.email === email)
+            const examineeArray = removeIf(examinees, examinee => examinee.email === email)
             // TODO: TODO: currently, to avoid "render fewer hooks than expected" error thrown from React,
             //  setting an empty array is a effective trick, but we need to know the root cause
             //  and use the more proper way instead.
             setExaminees([])
-            setExaminees(examineesArray)
+            setExaminees(examineeArray)
         }
 
         return (
--- src/utils/array.js
+++ src/utils/array.js
@@ -1,8 +1,8 @@
 export function removeIf(array, predicate) {
     for (let i = 0; i < array.length; i++) {
-        const removed = array.find(predicate);
-        const index = array.indexOf(removed);
-        array.splice(index, 1);
+        if (predicate(array[i])) {
+            array.splice(i, 1)
+        }
     }
     return array
 }

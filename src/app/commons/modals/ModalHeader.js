694cac9849306374499d66b4119bc3e472fdf83d
ningwang <nyngwang@gmail.com>
2021-05-14 13:08:25 +0800

    Fuckyou CSS.

--- src/app/commons/modals/ModalHeader.js
+++ src/app/commons/modals/ModalHeader.js
@@ -2,14 +2,9 @@ import * as React from "react";
 import './ModalHeader.css'
 
 const ModalHeader = function ({ title, style }) {
-    const { textAlign, color } = style
-
     return (
         <div>
-            <h1 className="modal-header font-poppins" style={{
-                textAlign: textAlign,
-                color: color
-            }}>
+            <h1 className="modal-header font-poppins" style={style}>
                 {title}
             </h1>
         </div>
--- src/app/exam/modals/RejudgeProblemModal.js
+++ src/app/exam/modals/RejudgeProblemModal.js
@@ -21,21 +21,21 @@ const RejudgeProblemModal = ({title, show, problemId, problemTitle, onClose, rej
 
     return renderModal({
         modalClassName: "rejudge-problem-modal",
-        modalWidth: "600px",
+        modalWidth: "620px",
         show: show === problemId,
         onClose, closeIconRef,
         contentRendering: () => (
             <div className="rejudge-problem-modal">
                 <form onSubmit={handleFormSubmit} ref={formRef}>
                     <div id="modal" className="font-poppins">
-                        <div className="mx-5 mt-2">
-                            <ModalHeader title={title} style={{textAlign: "left", color: "#FF8C00"}} />
+                        <div className="modal-header mt-2">
+                            <ModalHeader title={title}/>
                         </div>
-                        <div className="py-3" style={{ background: "#F5F5DC", fontSize: "21px" }}>
+                        <div className="rejudge-message py-3">
                             All the submissions under this problem will be rejudged.
                         </div>
-                        <div className="mx-6 my-5" style={{ fontSize: "20px", textAlign: "left" }}>
-                            Problem Title: <h3>{problemId} {problemTitle}</h3>
+                        <div className="placeholder mx-6 my-3">
+                            <h1>Problem Title: <span className="data my-1">{problemId} {problemTitle}</span></h1>
                         </div>
                         <div className="submit-btn mx-5">
                             <button className="button" id="add-btn" type="submit">

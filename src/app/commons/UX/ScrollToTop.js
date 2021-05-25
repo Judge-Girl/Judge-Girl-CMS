50c539c7f72be38dd515ec6fa42bbaded9b20347
ningwang <nyngwang@gmail.com>
2021-05-25 23:28:03 +0800

    Comment added.

--- src/app/commons/UX/ScrollToTop.js
+++ src/app/commons/UX/ScrollToTop.js
@@ -3,6 +3,8 @@ import {useEffect} from "react";
 
 
 export const ScrollToTop = () => {
+    /* The pathname will change on every page-transition,
+     * and we take advantage of this to trigger scroll-to-top. */
     const { pathname } = useLocation()
 
     useEffect(() => {

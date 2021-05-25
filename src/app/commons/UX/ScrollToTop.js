c8550b916db8b899426d5387652942d301543a26
ningwang <nyngwang@gmail.com>
2021-05-25 13:15:06 +0800

    Fixed the scroll to top issue.

---
+++ src/app/commons/UX/ScrollToTop.js
@@ -0,0 +1,13 @@
+import {useLocation} from "react-router-dom";
+import {useEffect} from "react";
+
+
+export const ScrollToTop = () => {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [pathname])
+
+    return null
+}

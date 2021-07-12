a905f4aaac8e945f762d8a35479d37758b77ffed
ningwang <nyngwang@gmail.com>
2021-07-12 16:06:48 +0800

    :bug: Fixed #159.

--- src/models/Question.js
+++ src/models/Question.js
@@ -1,9 +1,9 @@
 export default class Question {
-    constructor({examId, problemId, quota, maxScore, questionOrder}) {
+    constructor({examId, problemId, quota, score, questionOrder}) {
         this.examId = examId;
         this.problemId = problemId;
         this.quota = quota;
-        this.maxScore = maxScore;
+        this.score = score;
         this.questionOrder = questionOrder;
     }
 }

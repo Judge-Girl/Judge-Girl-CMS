c01bbf566273513375dae20b4b4bd911c2b7b5e7
ningwang <nyngwang@gmail.com>
2021-08-07 11:30:04 +0800

    Spacing of Right bar.

--- src/app/problem2/right/Block.js
+++ src/app/problem2/right/Block.js
@@ -2,12 +2,11 @@ const Block = ({title, id, titleButton, children}) => {
 
     return <>
         <div id={id} style={{width: "100%"}}>
-            <div style={{
-                display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
-                <div style={{fontSize: "21px", fontWeight: "700"}}>
+            <div className="block-title" style={{display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+                <div className="block-title-item" style={{fontSize: "21px", fontWeight: "700"}}>
                     {title}
                 </div>
-                <div>{titleButton}</div>
+                <div className="block-title-item">{titleButton}</div>
             </div>
             <div>
                 {children}

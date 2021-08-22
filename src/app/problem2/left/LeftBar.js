9b44484fd1973278e50882acc27613330f3b9478
Ning Wang <nyngwang@gmail.com>
2021-08-22 23:45:55 +0800

    Refactor: state-lifting; adding TODOs.

--- src/app/problem2/left/LeftBar.js
+++ src/app/problem2/left/LeftBar.js
@@ -1,13 +1,13 @@
 import "./leftbar.scss";
-import {useParams} from "react-router-dom";
 import {SECTIONS} from "./SectionManager";
+import {useParams} from "react-router-dom";
 
 
+// TODO: UX idea:
+//   Option1: Dim other sections, when the section is clicked.
+//   Option2: Blink the background of the target section.
 const LeftBar = () => {
-    // TODO: Use state to imple. the effect:
-    //   Option1: Dim other sections, when the section is clicked
-    //   Option2: Blink the background of the target section.
-    const {problemId} = useParams()
+    const {problemId} = useParams();
 
     const backToTop = e => {
         e.preventDefault();
@@ -16,29 +16,21 @@ const LeftBar = () => {
 
     const scrollToSection = (tagId, e) => {
         e.preventDefault();
-        const target = document.getElementById(tagId)
-        target && target.scrollIntoView({
-            behavior: 'smooth',
-            // block: 'center',
-            // inline: 'center'
-        });
+        document.getElementById(tagId)?.scrollIntoView({behavior: 'smooth'});
     };
 
     return <>
-        <div className="problem-editor-leftbar"
-             style={{
-                 display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "flex-end",
-                 position: "fixed", width: "200px",
-             }}>
-            <a href="#" onClick={backToTop}>
+        <div className="problem-editor-leftbar" style={{
+            display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "flex-end",
+            position: "fixed", width: "200px"}}>
+            <a key="#" href="#" onClick={backToTop}>
                 ID: {problemId}
             </a>
-            {SECTIONS.map(section =>
-                <a href={`#${section.id}`}
-                   onClick={e => scrollToSection(section.id, e)}>
-                    {section.name}
-                </a>)
-            }
+        {SECTIONS.map(section =>
+            <a key={section.id} href={`#${section.id}`} onClick={e => scrollToSection(section.id, e)}>
+                {section.name}
+            </a>)
+        }
         </div>
     </>;
 }
--- src/app/problem2/right/RightBar.js
+++ src/app/problem2/right/RightBar.js
@@ -1,9 +1,4 @@
 import "./rightbar.scss"
-import ProblemEditorTitle from "../../problem/edit/ProblemEditorTitle";
-import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {problemService} from "../../../services/services";
-import {Spinner} from "../../commons/Spinner";
 import Tags from "./Tags";
 import ProvidedCode from "./ProvidedCode";
 import SubmittedCodeSpec from "./SubmittedCodeSpec";
@@ -14,31 +9,13 @@ import Visible from "./Visible";
 import Description from "./Description";
 import TestCases from "./TestCases";
 import Actions from "./Actions";
+import ProblemTitle from "./ProblemTitle";
+
 
 const RightBar = () => {
-    const {problemId} = useParams();
-    const [currentProblem, setCurrentProblem] = useState(undefined);
-
-    const fetchProblem = (problemId) => {
-        problemService.getProblemById(problemId)
-            .then(problem => {
-                setCurrentProblem(problem);
-            });
-    };
-
-    useEffect(() => {
-        if (!currentProblem) {
-            fetchProblem(problemId);
-        }
-    }, [currentProblem, problemId]);
-
-    if (!currentProblem) {
-        return <Spinner/>;
-    }
-
     return <>
         <div className="problem-editor-rightbar">
-            <ProblemEditorTitle problem={currentProblem}/>
+            <ProblemTitle/><hr/>
             <Tags/><hr/>
             <ProvidedCode/><hr/>
             <SubmittedCodeSpec/><hr/>

f9fa24ec059e9409aaa857bbaa9b401c155f683b
ningwang <nyngwang@gmail.com>
2021-08-22 20:04:53 +0800

    Refactor the ProblemList: remove flag shouldRedirect.

--- src/app/problem/list/ProblemList.js
+++ src/app/problem/list/ProblemList.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {ItemListPage} from "../../commons/ItemListPage/ItemListPage";
 import {CreateButton} from "../../commons/buttons/CreateButton";
 import CreateProblemModal from "../Modal/CreateProblemModal";
@@ -12,84 +12,81 @@ import ProblemEditor2 from "../../problem2/ProblemEditor2";
 const ProblemList = () => {
     const [showCreateProblemModal, setShowCreateProblemModal] = useState(false);
     const [problems, setProblems] = useState(undefined);
+    // TODO: The currentProblemId is not reset to undefined when in ProblemEditor2.
     const [currentProblemId, setCurrentProblemId] = useState(undefined);
-    const [shouldRedirect, setShouldRedirect] = useState(false);
-
-    const onProblemCreated = (problemId) => {
-        fetchProblems();
-        setCurrentProblemId(problemId);
-        setShouldRedirect(true);
-    };
-
-    const fetchProblems = () => {
+    const fetchProblems = useCallback(problemId => {
         problemService.getAllProblems()
-            .then(problems => setProblems(problems));
-    }
+            .then(problems => {
+                setProblems(problems);
+                setCurrentProblemId(problemId);
+            });
+    }, [setProblems, setCurrentProblemId]);
 
     useEffect(() => {
-        if (!problems) {
+        if (!problems || problems.length === 0) {
             fetchProblems();
         }
-    });
-
-    if (!problems) {
-        return <Spinner/>
-    }
+    }, [problems, fetchProblems]);
 
     /* TODO: The commented code below is problematic, error message: "a component is changing an uncontrolled input to be controlled"
     if (shouldRedirect) {
         return (<Redirect to={`problems/${currentProblemId}/edit`}/>);
     }
-     */
+    This problem has been resolved by removing the unnecessary flag shouldRedirect.
+    This should be deleted after review.
+    */
+
+    if (!problems) {
+        return <Spinner/>;
+    }
 
     return (
-        <>
-            {shouldRedirect ? <Redirect to={`problems/${currentProblemId}/edit`}/> : ""}
-            <Route path="/problems" exact>
-                <div className="problem-list font-poppins">
-                    <div style={{paddingTop: "20px", paddingBottom: "150px"}}>
-                        <div style={{display: "flex", justifyContent: "center"}}>
-                            <ItemListPage title="Problem List"
-                                          width="1000px"
-                                          filterItems={["Filter", "Id", "tags"]}
-                                          Button={() =>
-                                              <CreateButton onClick={() => setShowCreateProblemModal(true)}/>}
-                                          tableHeaders={[
-                                              <TableCell>#</TableCell>,
-                                              <TableCell>Problem Title</TableCell>,
-                                              <TableCell>Tags</TableCell>
-                                          ]}
-                                          tableRowGenerator={{
-                                              list: problems,
-                                              key: (problem) => problem.id,
-                                              data: (problem) => [
-                                                  <TableCell>
-                                                      <p>{problem.id}</p>
-                                                  </TableCell>,
-                                                  <TableCell>
-                                                      <Link to={`/problems/${problem.id}/edit`}>
-                                                          {problem.title}</Link>
-                                                  </TableCell>,
-                                                  <TableCell>
-                                                      <span className="tag is-link">Functions</span>
-                                                  </TableCell>,
-                                              ]
-                                          }}
-                                          tableDataStyle={{textAlign: "left"}}/>
-                            <CreateProblemModal show={showCreateProblemModal}
-                                                onClose={() => setShowCreateProblemModal(false)}
-                                                onProblemCreated={problemId => onProblemCreated(problemId)}/>
+        <>{currentProblemId?
+            <Redirect to={`problems/${currentProblemId}/edit2`}/>: ""}
+            <Switch>
+                <Route path="/problems" exact>
+                    <div className="problem-list font-poppins">
+                        <div style={{paddingTop: "20px", paddingBottom: "150px"}}>
+                            <div style={{display: "flex", justifyContent: "center"}}>
+                                <ItemListPage title="Problem List"
+                                              width="1000px"
+                                              filterItems={["Filter", "Id", "tags"]}
+                                              Button={() =>
+                                                  <CreateButton onClick={() => setShowCreateProblemModal(true)}/>}
+                                              tableHeaders={[
+                                                  <TableCell>#</TableCell>,
+                                                  <TableCell>Problem Title</TableCell>,
+                                                  <TableCell>Tags</TableCell>
+                                              ]}
+                                              tableRowGenerator={{
+                                                  list: problems,
+                                                  key: (problem) => problem.id,
+                                                  data: (problem) => [
+                                                      <TableCell>
+                                                          <p>{problem.id}</p>
+                                                      </TableCell>,
+                                                      <TableCell>
+                                                          <Link to={`problems/${problem.id}/edit2`}>
+                                                              {problem.title}</Link>
+                                                      </TableCell>,
+                                                      <TableCell>
+                                                          <span className="tag is-link">Functions</span>
+                                                      </TableCell>,
+                                                  ]
+                                              }}
+                                              tableDataStyle={{textAlign: "left"}}/>
+                                <CreateProblemModal show={showCreateProblemModal}
+                                                    onClose={() => setShowCreateProblemModal(false)}
+                                                    onProblemCreated={problemId => fetchProblems(problemId)}/>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </Route>
-
-            <Switch>
+                </Route>
                 <Route path="/problems/:problemId/edit">
                     <ProblemEditor/>
                 </Route>
                 <Route path="/problems/:problemId/edit2">
-                    <ProblemEditor2/>
+                    <ProblemEditor2 clearParentFlag/>
                 </Route>
             </Switch>
         </>

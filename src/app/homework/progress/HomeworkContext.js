96698218fbd33b81f4189693536ef682c26eb559
提姆 <69762490+timt1028@users.noreply.github.com>
2021-09-19 11:31:50 +0800

    [UI] Homework page #140 (#162)

---
+++ src/app/homework/progress/HomeworkContext.js
@@ -0,0 +1,7 @@
+import {createContext, useContext} from "react";
+
+export const HomeworkContext = createContext(null);
+
+export const useHomeworkContext = () => {
+    return useContext(HomeworkContext);
+};

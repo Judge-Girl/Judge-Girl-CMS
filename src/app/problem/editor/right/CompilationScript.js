2080c3c0295dfceee6d3034950c5db8d3b818723
昭妤 Chao-Yu Lee <chaoyu2330@gmail.com>
2021-09-24 16:13:53 +0800

    Dev/177 Compilation script API integrate (#206)
    * Compilation script API integrate

--- src/app/problem/editor/right/CompilationScript.js
+++ src/app/problem/editor/right/CompilationScript.js
@@ -1,59 +1,84 @@
+import "./CompilationScript.scss"
 import Block from "./Block";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {EditSaveCancelButton} from "../../commons/EditSaveCancelButton";
 import {EditorButton} from "../../commons/EditorButton";
+import {useParams} from "react-router-dom";
+import {problemService} from "../../../../services/services";
+import {LanguageEnv} from "../../Model";
 
 
 const CompilationScript = () => {
+    const {problemId} = useParams();
+    const [problem, setProblem] = useState(undefined);
+    const [languageEnv, setLanguageEnv] = useState(undefined);
     const [isEditing, setIsEditing] = useState(false);
     const [script, setScript] = useState("");
+    const [scriptBackUp, setScriptBackUp] = useState(undefined);
+
+    useEffect(() => {
+        if (!problem) {
+            problemService.getProblemById(problemId)
+                .then(p => {
+                    setProblem(p);
+                    setLanguageEnv(new LanguageEnv(p.languageEnvs[0]));
+                });
+        }
+        if (languageEnv) {
+            setScript(languageEnv.getCompilationScript());
+        }
+    }, [languageEnv, problem, problemId]);
 
     const onClickEdit = () => {
         setIsEditing(true);
-    }
+        setScriptBackUp(script);
+    };
 
     const onClickSave = () => {
         setIsEditing(false);
-    }
+        languageEnv.updateCompilationScript(script);
+        problemService.updateLanguageEnv(problemId, languageEnv)
+            .then(() => {
+                console.log("The problem's compilation script has been updated");
+            });
+    };
 
     const onClickCancel = () => {
         setIsEditing(false);
-    }
+        setScript(scriptBackUp);
+    };
 
     return <>
         <Block title="Compilation Script"
                id="problem-editor-compilation-script"
                titleButton={
-               <EditSaveCancelButton
-                   isEditing={isEditing}
-                   onClickEdit={onClickEdit}
-                   onClickSave={onClickSave}
-                   onClickCancel={onClickCancel}/>
+                   <EditSaveCancelButton
+                       isEditing={isEditing}
+                       onClickEdit={onClickEdit}
+                       onClickSave={onClickSave}
+                       onClickCancel={onClickCancel}/>
                }>
-            {!isEditing?
-                <div style={{color: "rgba(18, 115, 186, 1)"}}>{script}</div>
-                :
-                <>
-                    {/* TODO: Should be moved to ProblemEditor.scss. */}
-                    {/* TODO: Default values should passed by variable. */}
-                    <textarea
-                        cols="40" rows="7"
-                        className="compile-script-text-area" style={{resize: "vertical"}}
-                        placeholder="gcc a.out -o main.c" value={script}
-                        onChange={e => setScript(e.target.value)}
-                    />
-                    <EditorButton
-                        text="Auto Generate"
-                        type="file"
-                        buttonColor="rgba(236, 112, 99, 1)"
-                        fontSize="16px"
-                        fontColor="#fff"
-                        width="11em"
-                        height="36px"
-                        borderRadius="50px"
-                        onClick={undefined} />
-                </>
-            }
+
+            <div className={"compilation-script"}>
+                {!isEditing ?
+                    <div>{script}</div>
+                    :
+                    <>
+                        <textarea className="compile-script-text-area"
+                                  placeholder="gcc a.out -o main.c"
+                                  value={script}
+                                  onChange={e => setScript(e.target.value)}/>
+                        <EditorButton text="Auto Generate"
+                                      type="file"
+                                      buttonColor="rgba(236, 112, 99, 1)"
+                                      fontSize="16px"
+                                      fontColor="#fff"
+                                      width="11em"
+                                      height="36px"
+                                      borderRadius="50px"
+                                      onClick={undefined}/>
+                    </>}
+            </div>
         </Block>
     </>;
 };

54c17db2611c03df651572e05d5bfcc9cc37ee5f
chrisLo <chrislo@hitrust.com.tw>
2021-09-26 17:09:52 +0800

    ExamScore Export report as CSV file

--- src/app/exam/score/ExamScorePage.js
+++ src/app/exam/score/ExamScorePage.js
@@ -1,14 +1,14 @@
-import {ItemListPage} from "../../commons/ItemListPage/ItemListPage";
-import {useParams} from "react-router-dom";
-import React, {useEffect, useState} from "react";
-import {Spinner} from "../../commons/Spinner";
+import { ItemListPage } from "../../commons/ItemListPage/ItemListPage";
+import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Spinner } from "../../commons/Spinner";
 import ExamSummary from "./ExamSummary";
-import {examService, examTranscriptService} from "../../../services/services";
-import {ExamScorePresenter} from "./ExamScorePresenter";
+import { examService, examTranscriptService } from "../../../services/services";
+import { ExamScorePresenter } from "./ExamScorePresenter";
 import './ExamScorePage.scss';
 
 const ExamScorePage = () => {
-    const {examId} = useParams();
+    const { examId } = useParams();
     const [presenter, setPresenter] = useState(undefined);
     const [exam, setExam] = useState(undefined);
 
@@ -28,54 +28,50 @@ const ExamScorePage = () => {
     }, [exam, setPresenter, examId]);
 
     if (!exam || !presenter) {
-        return <Spinner/>;
+        return <Spinner />;
     }
 
     const onButtonExportCSVClick = (examId) => {
         examService.getExamTranscriptCsvFile(examId)
-        .then((res) => {
-            const url = window.URL.createObjectURL(new Blob([res.data]));
-            const link = document.createElement('a');
-            link.href = url;
-            link.setAttribute('download', 'ExamTranscript.csv');
-            link.click();
-        });
+            .then((res) => {
+                const url = window.URL.createObjectURL(new Blob([res.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', 'ExamTranscript.csv');
+                link.click();
+            });
     };
 
     return (
         <div className="exam-score">
-            <div className="exam-score-font-poppins">
-                <div className="exam-score-mt-2">
-                    <div className="exam-score-mr-3">
-                        <ExamSummary vertical
-                                averageScore={presenter.crossAverage}
-                                maxScore={presenter.sumOfMaxScores}
-                                totalExaminees={presenter.totalExaminees} />
-                            <button className="export-blue-btn"
-                                onClick={() => onButtonExportCSVClick(examId)}>
-                                Export CSV
-                            </button>
-                    </div>
-                    <ItemListPage
-                        width="1000px"
-                        tableHeaders={[
-                            "Name",
-                            ...presenter.problemIds.map(problemId => `${problemId}`),
-                            "Table Score"
-                        ]}
-                        tableRowGenerator={{
-                            list: presenter.examineeRecords,
-                            key: examinee => examinee.studentId,
-                            data: ({studentName, studentScores, studentTotalScore}) => [
-                                <span>{studentName}</span>,
-                                ...studentScores.map(score => <span>{score}</span>),
-                                <span>{studentTotalScore} / {presenter.sumOfMaxScores}</span>,
-                            ]
-                        }}
-                        showFilterSearchBar={false}
-                    />
-                </div>
+            <div className="exam-score-left">
+                <ExamSummary vertical
+                    averageScore={presenter.crossAverage}
+                    maxScore={presenter.sumOfMaxScores}
+                    totalExaminees={presenter.totalExaminees} />
+                <button className="export-blue-btn"
+                    onClick={() => onButtonExportCSVClick(examId)}>
+                    Export CSV
+                </button>
             </div>
+            <ItemListPage
+                width="1000px"
+                tableHeaders={[
+                    "Name",
+                    ...presenter.problemIds.map(problemId => `${problemId}`),
+                    "Table Score"
+                ]}
+                tableRowGenerator={{
+                    list: presenter.examineeRecords,
+                    key: examinee => examinee.studentId,
+                    data: ({ studentName, studentScores, studentTotalScore }) => [
+                        <span>{studentName}</span>,
+                        ...studentScores.map(score => <span>{score}</span>),
+                        <span>{studentTotalScore} / {presenter.sumOfMaxScores}</span>,
+                    ]
+                }}
+                showFilterSearchBar={false}
+            />
         </div>
     )
 };

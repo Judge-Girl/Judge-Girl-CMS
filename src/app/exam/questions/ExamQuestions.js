af7e8d0ec1e01db2e320706495e26dd09816b011
wally5077 <wally55077@gmail.com>
2021-10-09 19:29:26 +0800

    :sparkles: [ExamQuestion] Change exam questions by mouse scrolling

--- src/app/exam/questions/ExamQuestions.js
+++ src/app/exam/questions/ExamQuestions.js
@@ -95,6 +95,31 @@ const ExamQuestions = () => {
         return <Spinner/>;
     }
 
+    const ondragQuestion = (e, dragExamQuestion) => {
+        e.dataTransfer.setData('dragQuestionId', dragExamQuestion.problemId);
+    };
+
+    const ondropQuestion = (e, dropExamQuestion) => {
+        e.preventDefault();
+        const dragQuestionIndex = questions.findIndex(question => question.problemId === parseInt(e.dataTransfer.getData('dragQuestionId')));
+        const dropQuestionIndex = questions.findIndex(question => question.problemId === dropExamQuestion.problemId);
+        const dragQuestion = questions[dragQuestionIndex];
+        const dropQuestion = questions[dropQuestionIndex];
+        questions.splice(dragQuestionIndex, 1, dropQuestion);
+        questions.splice(dropQuestionIndex, 1, dragQuestion);
+        const editQuestions = [];
+        questions.forEach((question, index) => {
+            const questionOrder = index + 1;
+            if (question.questionOrder !== questionOrder) {
+                question.questionOrder = questionOrder;
+                editQuestions.push({examId, problemId: question.problemId, questionOrder: question.questionOrder});
+            }
+        });
+        examService.editExamQuestions(examId, editQuestions)
+            .then(() => setQuestions([...questions]))
+            .catch((error) => console.log(`update question's order fail: ${error.message}`));
+    };
+
     return (
         <div className="exam-questions font-poppins">
             <div className="exam-content">
@@ -141,7 +166,10 @@ const ExamQuestions = () => {
                             </div>
                         ]
                     }}
-                    showFilterSearchBar={false}/>
+                    showFilterSearchBar={false}
+                    draggable={true}
+                    ondrag={ondragQuestion}
+                    ondrop={ondropQuestion}/>
                 <div className="add-question-btn"
                      onClick={() => setShowAddQuestionModal(true)}>
                     <span>Add New Question</span>

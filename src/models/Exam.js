c0b3169dad5ad738b36848ef55dbba8e132aa81c
wally5077 <wally55077@gmail.com>
2021-11-06 17:49:35 +0800

    :sparkles: [Bug] The ExamWhiteList component can't be rendered in the ExamOption component

--- src/models/Exam.js
+++ src/models/Exam.js
@@ -1,10 +1,11 @@
 export default class Exam {
-  constructor({id, name, description, startTime, endTime, questions}) {
+  constructor({id, name, description, startTime, endTime, questions, whitelist}) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.startTime = startTime;
     this.endTime = endTime;
     this.questions = questions;
+    this.whitelist = whitelist;
   }
 }

4700c9de34861566dfcc7a871f2c2bf35c7f9873
wally5077 <wally55077@gmail.com>
2021-11-08 12:47:37 +0800

    :sparkles: Dev/232 The ExamWhiteList component can't be rendered in the ExamOption component

--- src/app/exam/options/ExamWhiteList.js
+++ src/app/exam/options/ExamWhiteList.js
@@ -13,8 +13,7 @@ const ExamWhiteList = ({whitelist, onWhitelistChange}) => {
   }, [ipAddresses]);
 
   const onAddIpAddress = (ipAddress) => {
-    const ipAddressFormat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    if(ipAddressFormat.test(ipAddress)) {
+    if(ipAddress?.match('\\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.|$)){4}\\b')) {
       addIpAddress(ipAddress);
     }
   };
